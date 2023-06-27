ceaa3af66f598e7c47c4a5d36a471ac22268c7b2
Elinia <asdfjhc@gmail.com>
2023-06-27 16:20:29 +0900

    style(client): make player board more friendly to human

--- client/src/routes/Me.svelte
+++ client/src/routes/Me.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  import { BuildMethod, type GizmoLevel } from 'gizmos-env/common'
+  import { BuildMethod } from 'gizmos-env/common'
   import { ActionType } from 'gizmos-env/GizmosEnv'
-  import type { BuildSolution, PlayerInfo } from 'gizmos-env/Player'
+  import type { BuildSolution } from 'gizmos-env/Player'
   import type { GizmosGame } from '$lib/game.js'
   import type { GizmosClient } from '$lib/client.js'
   import Gizmo from './Gizmo.svelte'
@@ -22,79 +22,96 @@
   const { me, env, is_avail } = game
 </script>
 
-{#if $env && $me}
-  {@const my_gizmos = [
-    { label: '➕Upgrade', gizmos: $me.upgrade_gizmos },
-    { label: '→Converter', gizmos: $me.converter_gizmos },
-    { label: '📁File', gizmos: $me.file_gizmos },
-    { label: '👌Pick', gizmos: $me.pick_gizmos },
-    { label: '🔧Build', gizmos: $me.build_gizmos },
-  ]}
-  <div>Point token: {$me.point_token}</div>
-  <div class="flex gap-2">
-    <div class="energy">
-      Energy ({$me.total_energy_num}/{$me.max_energy_num}):
-      <Energy energy_num={$me.energy_num} />
+<div class="min-w-[60em] rounded-md bg-blue-200 m-2 p-2 flex flex-col gap-2">
+  {#if $env && $me}
+    {@const my_gizmos = [
+      { label: '➕Upgrade', gizmos: $me.upgrade_gizmos },
+      { label: '→Converter', gizmos: $me.converter_gizmos },
+      { label: '📁File', gizmos: $me.file_gizmos },
+      { label: '👌Pick', gizmos: $me.pick_gizmos },
+      { label: '🔧Build', gizmos: $me.build_gizmos },
+    ]}
+    <div class="flex gap-2">
+      <div class="font-bold" title="estimated score">🏆: {$me.score}</div>
+      <div title="Gizmos count">
+        🏅: {$me.gizmos.length}/{$env.max_gizmos_num}
+      </div>
+      <div title="Level3 Gizmos count">
+        🥉: {$me.level3_gizmos.length}/{$env.max_level3_gizmos_num}
+      </div>
+      <div>⭐: {$me.point_token}</div>
+      <div class="energy" title="energy">
+        🔮 ({$me.total_energy_num}/{$me.max_energy_num}):
+        <Energy energy_num={$me.energy_num} />
+      </div>
     </div>
-  </div>
-  <div>
-    File ({$me.filed.length}/{$me.max_file_num > 0
-      ? $me.max_file_num
-      : 'forbidden'}):
-    <div class="gizmos">
-      {#each $me.filed as g}
-        {@const gizmo = $env.gizmo(g.id)}
-        {@const solutions = $is_avail[ActionType.BUILD_FROM_FILED]
-          ? $env.build_solutions(gizmo, BuildMethod.FROM_FILED)
-          : []}
-        {@const can_build = solutions.length > 0}
+    <div class="flex gap-2">
+      <div class="flex flex-col gap-2 border-2 border-blue-300 p-2 rounded-md">
         <div>
-          <Gizmo info={gizmo} />
-          <button
-            class:avail={can_build}
-            disabled={!can_build}
-            on:click={() =>
-              show_build_dialog(gizmo.id, BuildMethod.FROM_FILED, solutions)}
-          >
-            🔧
-          </button>
+          Filed Gizmos ({$me.filed.length}/{$me.max_file_num > 0
+            ? $me.max_file_num
+            : 'forbidden'})
         </div>
-      {/each}
-    </div>
-  </div>
-  <div>
-    Gizmos ({$me.gizmos.length}/{$env.max_gizmos_num} Level3 {$me.level3_gizmos
-      .length}/{$env.max_level3_gizmos_num}) :
-    {#each my_gizmos as { label, gizmos }}
-      <div>{label}:</div>
-      <div class="gizmos-simple">
-        {#each gizmos as gizmo}
-          {@const can_use = $is_avail[ActionType.USE_GIZMO] && gizmo.active}
-          <button
-            class:avail={can_use}
-            disabled={!can_use}
-            on:click={() => use_gizmo(gizmo.id)}
-          >
-            <Gizmo info={gizmo} />
-          </button>
+        <div class="flex flex-col gap-2">
+          {#each $me.filed as g}
+            {@const gizmo = $env.gizmo(g.id)}
+            {@const solutions = $is_avail[ActionType.BUILD_FROM_FILED]
+              ? $env.build_solutions(gizmo, BuildMethod.FROM_FILED)
+              : []}
+            {@const can_build = solutions.length > 0}
+            <div>
+              <Gizmo info={gizmo} />
+              <button
+                class:avail={can_build}
+                disabled={!can_build}
+                on:click={() =>
+                  show_build_dialog(
+                    gizmo.id,
+                    BuildMethod.FROM_FILED,
+                    solutions
+                  )}
+              >
+                🔧
+              </button>
+            </div>
+          {/each}
+        </div>
+      </div>
+      <div
+        class="grid grid-cols-5 gap-2 border-2 border-blue-300 p-2 rounded-md"
+      >
+        {#each my_gizmos as { label, gizmos }}
+          <div class="flex flex-col gap-2">
+            <div>{label}</div>
+            {#each gizmos as gizmo}
+              {@const can_use = $is_avail[ActionType.USE_GIZMO] && gizmo.active}
+              <button
+                class:avail={can_use}
+                disabled={!can_use}
+                on:click={() => use_gizmo(gizmo.id)}
+              >
+                <Gizmo info={gizmo} />
+              </button>
+            {/each}
+          </div>
         {/each}
       </div>
-    {/each}
-  </div>
-  <div>
-    <button
-      class:avail={$is_avail[ActionType.GIVE_UP]}
-      disabled={!$is_avail[ActionType.GIVE_UP]}
-      on:click={() => give_up()}
-    >
-      Give up
-    </button>
-    <button
-      class:avail={$is_avail[ActionType.END]}
-      disabled={!$is_avail[ActionType.END]}
-      on:click={() => end()}
-    >
-      End turn
-    </button>
-  </div>
-{/if}
+    </div>
+    <div>
+      <button
+        class:avail={$is_avail[ActionType.GIVE_UP]}
+        disabled={!$is_avail[ActionType.GIVE_UP]}
+        on:click={() => give_up()}
+      >
+        Give up
+      </button>
+      <button
+        class:avail={$is_avail[ActionType.END]}
+        disabled={!$is_avail[ActionType.END]}
+        on:click={() => end()}
+      >
+        End turn
+      </button>
+    </div>
+  {/if}
+</div>
