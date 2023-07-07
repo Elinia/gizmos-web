3e6483ae7b9ada8a4dfebb14a0d5a22e38843d0e
Elinia <asdfjhc@gmail.com>
2023-07-07 14:51:00 +0900

    chore(client): hide auto function

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -24,76 +24,65 @@
     use_gizmo,
     give_up,
     end,
-    sample,
     replay,
   } = client
 
-  const { player_index, env, observation } = game
+  const { observation } = game
 
   let name = `player${random_int(100)}`
 
   onDestroy(() => {
     client.destroy()
   })
-
-  let auto = false
-  $: if (
-    auto &&
-    $env &&
-    $player_index === $observation?.curr_player_index &&
-    $env.action_space.length > 0
-  ) {
-    console.log(sample())
-  }
 </script>
 
-<div>
-  Connection status: <span
-    class:status-red={$socket_status === 'red'}
-    class:status-green={$socket_status === 'green'}
-  >
-    {$socket_status}
-  </span>
-</div>
-<div>
-  auto
-  <input type="checkbox" bind:value={auto} />
-</div>
-{#if !$observation}
-  <input bind:value={name} />
-  <button class="btn" on:click={() => login(name)}>login as {name}</button>
-  {#if $in_room}
-    <button class="btn" on:click={() => ready()}>ready</button>
+<div class="flex flex-col gap-2">
+  <div>
+    Connection status: <span
+      class:status-red={$socket_status === 'red'}
+      class:status-green={$socket_status === 'green'}
+    >
+      {$socket_status}
+    </span>
+  </div>
+  {#if !$observation}
+    <div>
+      <input bind:value={name} />
+      <button class="btn" on:click={() => login(name)}>login as {name}</button>
+      {#if $in_room}
+        <button class="btn" on:click={() => ready()}>ready</button>
+      {/if}
+    </div>
+    <ul>
+      {#each $room_info as info}
+        <li>{info.name} {info.ready ? '(ready)' : ''}</li>
+      {/each}
+      <li />
+    </ul>
   {/if}
-  <ul>
-    {#each $room_info as info}
-      <li>{info.name} {info.ready ? '(ready)' : ''}</li>
-    {/each}
-    <li />
-  </ul>
-{/if}
-<Game
-  ongoing={$game_ongoing}
-  {game}
-  {pending}
-  {pick}
-  {file}
-  {file_from_research}
-  {build}
-  {build_from_filed}
-  {build_from_research}
-  {research}
-  {use_gizmo}
-  {give_up}
-  {end}
-/>
-{#if $replay}
-  {@const blob = new Blob([JSON.stringify($replay, null, 2)], {
-    type: 'application/json ',
-  })}
-  {@const url = URL.createObjectURL(blob)}
-  <a href={url} download="replay.json">replay</a>
-{/if}
+  <Game
+    ongoing={$game_ongoing}
+    {game}
+    {pending}
+    {pick}
+    {file}
+    {file_from_research}
+    {build}
+    {build_from_filed}
+    {build_from_research}
+    {research}
+    {use_gizmo}
+    {give_up}
+    {end}
+  />
+  {#if $replay}
+    {@const blob = new Blob([JSON.stringify($replay, null, 2)], {
+      type: 'application/json ',
+    })}
+    {@const url = URL.createObjectURL(blob)}
+    <a href={url} download="replay.json">replay</a>
+  {/if}
+</div>
 
 <style lang="postcss">
   .status-red {
