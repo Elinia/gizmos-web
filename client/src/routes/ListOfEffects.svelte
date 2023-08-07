44ea141adaf77f9a8fcebd1e4791366e44b9a980
Elinia <asdfjhc@gmail.com>
2023-08-07 15:09:36 +0900

    misc(client): add chinese version of rulebook and list of effects

--- client/src/routes/ListOfEffects.svelte
+++ client/src/routes/ListOfEffects.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { onMount } from 'svelte'
-  import { _ } from 'svelte-i18n'
+  import { _, locale } from 'svelte-i18n'
 
   let dialog: HTMLDialogElement
 
@@ -23,20 +23,27 @@
 
 <button on:click={show_rule_book}>{$_('list_of_effects')}</button>
 <dialog bind:this={dialog}>
-  <object
-    title="list_of_effects"
-    data="/ListOfEffects.pdf#toolbar=0"
-    id="pdf"
-    width="100%"
-    height="100%"
-  >
-    <p>{$_('pdf_unsupported')}</p>
-    <p>
-      <a href="/ListOfEffects.pdf" download="ListOfEffects.pdf"
-        >{$_('download')}</a
-      >
-    </p>
-  </object>
+  {#if $locale === 'en-US'}
+    <object
+      title="list_of_effects"
+      data="/ListOfEffects.pdf#toolbar=0"
+      id="pdf"
+      width="100%"
+      height="100%"
+    >
+      <p>{$_('pdf_unsupported')}</p>
+      <p>
+        <a href="/ListOfEffects.pdf" download="ListOfEffects.pdf"
+          >{$_('download')}</a
+        >
+      </p>
+    </object>
+  {:else if $locale === 'zh-CN'}
+    <div class="flex flex-col gap-2">
+      <img src="/e1.jpeg" alt="list of effects 1" />
+      <img src="/e2.jpeg" alt="list of effects 2" />
+    </div>
+  {/if}
 </dialog>
 
 <style lang="postcss">
--- client/src/routes/RuleBook.svelte
+++ client/src/routes/RuleBook.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { onMount } from 'svelte'
-  import { _ } from 'svelte-i18n'
+  import { _, locale } from 'svelte-i18n'
 
   let dialog: HTMLDialogElement
 
@@ -23,18 +23,27 @@
 
 <button on:click={show_rule_book}>{$_('rulebook')}</button>
 <dialog bind:this={dialog}>
-  <object
-    title="rulebook"
-    data="/Rulebook.pdf#toolbar=0"
-    id="pdf"
-    width="100%"
-    height="100%"
-  >
-    <p>{$_('pdf_unsupported')}</p>
-    <p>
-      <a href="/Rulebook.pdf" download="Rulebook.pdf">{$_('download')}</a>
-    </p>
-  </object>
+  {#if $locale === 'en-US'}
+    <object
+      title="rulebook"
+      data="/Rulebook.pdf#toolbar=0"
+      id="pdf"
+      width="100%"
+      height="100%"
+    >
+      <p>{$_('pdf_unsupported')}</p>
+      <p>
+        <a href="/Rulebook.pdf" download="Rulebook.pdf">{$_('download')}</a>
+      </p>
+    </object>
+  {:else if $locale === 'zh-CN'}
+    <div class="flex flex-col gap-2">
+      <img src="/1.jpeg" alt="rulebook1" />
+      <img src="/2.jpeg" alt="rulebook2" />
+      <img src="/3.jpeg" alt="rulebook3" />
+      <img src="/4.jpeg" alt="rulebook4" />
+    </div>
+  {/if}
 </dialog>
 
 <style lang="postcss">
